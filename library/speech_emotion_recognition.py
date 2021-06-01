a740df614fcfdee2890ea8f090d6ebc4d85fa32d
restacksyj <theandroiddeveloper.yj@gmail.com>
2021-06-02 02:35:07 +0530

    updated requirements

--- library/speech_emotion_recognition.py
+++ library/speech_emotion_recognition.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 ## Audio Preprocessing ##
-import pyaudio
+# import pyaudio
 import wave
 import librosa
 from scipy.stats import zscore
@@ -37,57 +37,57 @@ class speechEmotionRecognition:
     '''
     Voice recording function
     '''
-    def voice_recording(self, filename,filename2, duration=5, sample_rate=16000, chunk=1024, channels=1):
-
-        # Start the audio recording stream
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16,
-                        channels=channels,
-                        rate=sample_rate,
-                        input=True,
-                        frames_per_buffer=chunk)
-
-        # Create an empty list to store audio recording
-        frames = []
-
-        # Determine the timestamp of the start of the response interval
-        print('* Start Recording *')
-        stream.start_stream()
-        start_time = time.time()
-        current_time = time.time()
-
-        # Record audio until timeout
-        while (current_time - start_time) < duration:
-
-            # Record data audio data
-            data = stream.read(chunk)
-
-            # Add the data to a buffer (a list of chunks)
-            frames.append(data)
-
-            # Get new timestamp
-            current_time = time.time()
-
-        # Close the audio recording stream
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-        print('* End Recording * ')
-
-        # Export audio recording to wav format
-        wf = wave.open(filename, 'w')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-        wf.setframerate(sample_rate)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-
-        wf = wave.open(filename2, 'w')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-        wf.setframerate(sample_rate)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    # def voice_recording(self, filename,filename2, duration=5, sample_rate=16000, chunk=1024, channels=1):
+    #
+    #     # Start the audio recording stream
+    #     p = pyaudio.PyAudio()
+    #     stream = p.open(format=pyaudio.paInt16,
+    #                     channels=channels,
+    #                     rate=sample_rate,
+    #                     input=True,
+    #                     frames_per_buffer=chunk)
+    #
+    #     # Create an empty list to store audio recording
+    #     frames = []
+    #
+    #     # Determine the timestamp of the start of the response interval
+    #     print('* Start Recording *')
+    #     stream.start_stream()
+    #     start_time = time.time()
+    #     current_time = time.time()
+    #
+    #     # Record audio until timeout
+    #     while (current_time - start_time) < duration:
+    #
+    #         # Record data audio data
+    #         data = stream.read(chunk)
+    #
+    #         # Add the data to a buffer (a list of chunks)
+    #         frames.append(data)
+    #
+    #         # Get new timestamp
+    #         current_time = time.time()
+    #
+    #     # Close the audio recording stream
+    #     stream.stop_stream()
+    #     stream.close()
+    #     p.terminate()
+    #     print('* End Recording * ')
+    #
+    #     # Export audio recording to wav format
+    #     wf = wave.open(filename, 'w')
+    #     wf.setnchannels(channels)
+    #     wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    #     wf.setframerate(sample_rate)
+    #     wf.writeframes(b''.join(frames))
+    #     wf.close()
+    #
+    #     wf = wave.open(filename2, 'w')
+    #     wf.setnchannels(channels)
+    #     wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    #     wf.setframerate(sample_rate)
+    #     wf.writeframes(b''.join(frames))
+    #     wf.close()
 
 
     '''
